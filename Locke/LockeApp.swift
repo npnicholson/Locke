776d51f5ce38f9ed215aa630100f1619930b6450
Norris Nicholson <norrispnicholson@gmail.com>
2023-08-14 14:36:45 -0400

    Initial Commit

---
+++ Locke/LockeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockeApp.swift
+//  Locke
+//
+//  Created by Norris Nicholson on 8/14/23.
+//
+
+import SwiftUI
+
+@main
+struct LockeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
